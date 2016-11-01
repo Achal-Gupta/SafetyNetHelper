5748b211fd56384869c45a5b980e6573b581e617
Jared Rummler <jared.rummler@gmail.com>
2016-10-31 17:02:05 -0700

    Update copyright

--- library/src/main/java/com/jrummyapps/android/safetynet/SafetyNetHelper.java
+++ library/src/main/java/com/jrummyapps/android/safetynet/SafetyNetHelper.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2016 Jared Rummler <jared.rummler@gmail.com>
- * Copyright (C) 2015 Scott Alexander-Bown
+ * Copyright (C) 2016 JRummy Apps Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Raw
  */
 
 package com.jrummyapps.android.safetynet;
